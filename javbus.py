49dade91005c7714b0dfd7c3e84bf0e15aa27dd0
MyFaith <faith0725@outlook.com>
2017-03-03 22:08:38 +0800

    修改为多线程

--- javbus.py
+++ javbus.py
@@ -1,51 +1,43 @@
 # coding: utf-8
 
-import requests, re, math, random
+import requests, re, math, random, time
 from bs4 import BeautifulSoup
-from progress.bar import ShadyBar
 from pymongo import MongoClient
+import threading
+import queue
 
-mongo = MongoClient()
+mongo = MongoClient(host='192.168.199.217')
 db = mongo.javbus
+mutex = threading.Lock()
 
-class Javbus():
-    def __init__(self, filepath='av.txt', timeout=5000):
-        self.url = 'https://www.javbus.in/page/{page}'
-        self.filepath = filepath
-        self.timeout = timeout
-        self.total_page = 93
-        self.avs_info = []
-        self.final_data = []
+class Javbus(threading.Thread):
+    def __init__(self, page_queue):
+        self.page_queue = page_queue
+        self.avs_queue = queue.Queue()
         self.s = requests.Session()
         self.header = {
-            'Referer': 'http://www.javbus.in',
+            'Referer': 'http://www.javbus.com',
             'Cookie': 'existmag=all',
             'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
         }
+        threading.Thread.__init__(self)
 
-    def get_html(self, page):
-        url = self.url.replace('{page}', str(page))
-        req = self.s.get(url, headers=self.header, timeout=self.timeout)
-        return req.text
+    def run(self):
+        while not self.page_queue.empty():
+            url = 'http://www.javbus.com/page/%s' %self.page_queue.get()
+            self.get_datas(url)
+            time.sleep(2)
 
-    def get_full_page(self):
-        full_page = ''
-        bar = ShadyBar('fetching', max=self.total_page, suffix='%(percent)d%% [%(index)d/%(max)d]')
-        for index in range(self.total_page):
-            text = self.get_html(index+1)
-            full_page += text
-            bar.next()
-        bar.finish()
-        return full_page
-
-    def get_all_fh(self):
-        full_page = self.get_full_page()
-        soup = BeautifulSoup(full_page, 'html.parser')
+    def get_datas(self, url):
+        avs = []
+        # 获取某一页的HTML
+        print('正在获取 %s 的数据...' %url)
+        html = self.s.get(url, headers=self.header).text
+        soup = BeautifulSoup(html, 'html.parser')
         divs = soup.find_all(class_='item')
-        bar = ShadyBar('getting fh', max=len(divs), suffix='%(percent)d%% [%(index)d/%(max)d]')
+        # 获取基本数据
         for item in divs:
             av = item.find(class_='photo-info')
-            # 需要的数据
             title = item.find(class_='photo-frame').img['title']
             fh = av.span.date.next
             time = av.span.date.next.next.next.next
@@ -56,16 +48,11 @@ class Javbus():
                 'time': time,
                 'link': link
             }
-            self.avs_info.append(info)
-            bar.next()
-        bar.finish()
-        return self.avs_info
-
-    def get_magnet(self):
-        bar = ShadyBar('getting magnet', max=len(self.avs_info), suffix='%(percent)d%% [%(index)d/%(max)d]')
-        for item in self.avs_info:
+            avs.append(info)
+        # 获取磁链
+        for item in avs:
             url = item['link']
-            html = self.s.get(url, headers=self.header, timeout=self.timeout).text
+            html = self.s.get(url, headers=self.header).text
             # 由于磁力链接是ajax方式获取，所以获取数据，构成ajax链接
             gid = re.search(r'var gid = (\d*?);', html).group(1)
             lang = 'zh'
@@ -74,7 +61,7 @@ class Javbus():
             floor = math.floor(random.random() * 1e3 + 1)
             # 请求数据
             ajax_url = 'https://www.javbus.in/ajax/uncledatoolsbyajax.php?gid=%s&lang=%s&img=%s&uc=%s&floor=%s' %(gid, lang, img, uc, floor)
-            ajax_result = self.s.get(ajax_url, headers=self.header, timeout=self.timeout)
+            ajax_result = self.s.get(ajax_url, headers=self.header)
             soup = BeautifulSoup(ajax_result.text, 'html.parser')
             try:
                 magnet = soup.find('td').a['href']
@@ -83,6 +70,13 @@ class Javbus():
             # append
             item['img'] = img
             item['magnet'] = magnet
+            print('[取到数据]标题：%s 番号：%s 时间：%s 图片：%s 链接：%s 磁链：%s' %(item['title'], item['fh'], item['time'], item['img'], item['link'], item['magnet']))
+            self.avs_queue.put(item)
+        # 存储数据
+        mutex.acquire()
+        while not self.avs_queue.empty():
+            item = self.avs_queue.get()
+            print('[写入数据库]%s' %item['title'])
             db.avs.insert({
                 'title': item['title'],
                 'fh': item['fh'],
@@ -91,11 +85,27 @@ class Javbus():
                 'link': item['link'],
                 'magnet': item['magnet']
             })
-            self.final_data.append(item)
-            bar.next()
-        bar.finish()
+        mutex.release()
+
+def main():
+    # 构建页面队列
+    page_queue = queue.Queue()
+    for page in range(1, 5):
+        page_queue.put(page)
+    
+    threads = []
+    # 开启4个线程
+    for i in range(4):
+        javbus = Javbus(page_queue)
+        javbus.setDaemon(True)
+        javbus.start()
+        threads.append(javbus)
+    # 判断
+    while True:
+        for i in threads:
+            if not i.isAlive():
+                break
+        time.sleep(1)
 
 if __name__ == '__main__':
-    javbus = Javbus()
-    javbus.get_all_fh()
-    javbus.get_magnet()
+    main()
